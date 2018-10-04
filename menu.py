1dce36a3d00fc7cbc86ddb2f855585ba51977230
Elmer Sevillano <esevillano@ymail.com>
2018-10-04 13:37:34 -0700

    Add sprites and audio for the game

---
+++ menu.py
@@ -0,0 +1,10 @@
+class Menu:
+    """Initialize the start up menu for the game."""
+    def __init__(self):
+        print("hello")
+
+    def start_menu(self):
+        print("hello")
+
+    def high_scores(self):
+        print("High scores")
